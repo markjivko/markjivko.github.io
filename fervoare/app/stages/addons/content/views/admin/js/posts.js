b9da623b3d841b1b96287b04e77ae9caf8c164a5
Stephino <50949149+Stephino@users.noreply.github.com>
2021-07-19 17:29:34 +0300

    [chore] Minified assets

--- fervoare/app/stages/addons/content/views/admin/js/posts.js
+++ fervoare/app/stages/addons/content/views/admin/js/posts.js
@@ -1,34 +1,38 @@
 /* global fervoare */
-$(document).ready(function(){
-    var set_slug = function(){
+$(document).ready(function () {
+    var set_slug = function () {
         $('#slug_is_blog').css(
-            'display',
-            $('[name="type"]').val() === '1'?'inline':'none'
-        );
+                'display',
+                $('[name="type"]').val() === '1' ? 'inline' : 'none'
+                );
         $('#slug_link').attr(
-            'href',
-            $('#slug_before').html()
+                'href',
+                $('#slug_before').html()
                 + ($('[name="type"]').val() === '1' ? $('#slug_is_blog').html() : '')
                 + $('[name="slug"]').val()
-        );
+                );
     };
-    
+
     // Set the slug
-    set_slug();$('[name="type"],[name="slug"]').change(function(){set_slug();$('#slug_link').css('display','none');});
-    
+    set_slug();
+    $('[name="type"],[name="slug"]').change(function () {
+        set_slug();
+        $('#slug_link').css('display', 'none');
+    });
+
     // Append the update functionality
-    fervoare.toolbox.post_update = function(h,c){
-        if(typeof c.slug !== 'undefined') {
+    fervoare.toolbox.post_update = function (h, c) {
+        if (typeof c.slug !== 'undefined') {
             $('[name="slug"]').val(c.slug);
             set_slug();
         }
-        if(typeof c.title !== 'undefined') {
+        if (typeof c.title !== 'undefined') {
             $('[name="title"]').val(c.title);
         }
-        if(typeof c.status !== 'undefined') {
-            $('#slug_link').css('display',c.status === '1'?'inline-block':'none');
+        if (typeof c.status !== 'undefined') {
+            $('#slug_link').css('display', c.status === '1' ? 'inline-block' : 'none');
         }
-        if(typeof c.content !== 'undefined') {
+        if (typeof c.content !== 'undefined') {
             $('textarea#content.edit').val(c.content);
         }
     };
