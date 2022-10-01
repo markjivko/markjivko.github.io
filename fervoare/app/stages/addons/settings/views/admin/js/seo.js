6cfcb8db0b75141c3fa8903d5e5886b6233569a3
Mark Jivko <50949149+markjivko@users.noreply.github.com>
2022-10-01 09:16:20 +0300

    [chore] Moved fervoare docs to https://fervoare.com

--- fervoare/app/stages/addons/settings/views/admin/js/seo.js
+++
@@ -1,8 +0,0 @@
-/* global fervoare */
-$(document).ready(function () {
-    fervoare.toolbox.seo_update = function (h, c) {
-        $.each(c, function (k, v) {
-            $('#' + k).val(v);
-        });
-    };
-});
