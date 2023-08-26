d2f65f011250e898f6906001c4f56d9738d01ee1
Mark Jivko <50949149+markjivko@users.noreply.github.com>
2023-08-26 13:18:47 +0300

    [code] Added multiple layouts

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    arrowParens: "avoid",
+    bracketSpacing: true,
+    htmlWhitespaceSensitivity: "css",
+    insertPragma: false,
+    bracketSameLine: true,
+    jsxSingleQuote: false,
+    printWidth: 130,
+    proseWrap: "preserve",
+    quoteProps: "consistent",
+    requirePragma: false,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "none",
+    useTabs: false,
+};
--- post/functions.js
+++ post/functions.js
@@ -1,18 +1,45 @@
-const inputPadding = 20;
-const inputLeft = 110;
-const inputTop = 350;
-const startStretch = 180;
-
 document.addEventListener("DOMContentLoaded", function () {
-    let fontSize = 60;
-    let lineHeight = 70;
-
-    const input = document.querySelector('[data-role="input"] textarea');
+    const holder = document.querySelector(".holder");
+    const strechCheckbox = document.querySelector("input[name='stretch']");
     const canvas = document.querySelector('[data-role="output"]');
-    input.style.fontSize = `${fontSize}px`;
-    input.style.lineHeight = `${lineHeight}px`;
+    const textarea = document.querySelector('[data-role="input"] textarea');
+    const textareaHeight = textarea.offsetHeight;
+    let canvasBkg = null;
+    let canvasColor = null;
+    let canvasHeight = null;
+    let canvasWidth = null;
+    let fontSize = null;
+    let lineHeight = null;
+    let textareaLeft = null;
+    let textareaTop = null;
+    let textareaPadding = null;
+    let textareaStretch = strechCheckbox.checked;
 
-    document.querySelector("body").addEventListener("keyup", (e) => {
+    /**
+     * Recalculate style
+     */
+    const recalculateStyle = () => {
+        // Canvas dimensions
+        const canvasStyle = window.getComputedStyle(canvas);
+        canvasBkg = canvasStyle.backgroundColor;
+        canvasColor = canvasStyle.color;
+        canvasWidth = parseFloat(canvasStyle.width);
+        canvasHeight = parseFloat(canvasStyle.height);
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+
+        // Textarea dimensions and style
+        const textareaStyle = window.getComputedStyle(textarea);
+        fontSize = parseFloat(textareaStyle.fontSize);
+        lineHeight = parseFloat(textareaStyle.lineHeight);
+        textareaLeft = parseFloat(textareaStyle.left);
+        textareaTop = parseFloat(textareaStyle.top);
+        textareaPadding = parseFloat(textareaStyle.paddingLeft);
+    };
+    recalculateStyle();
+
+    // Change font size
+    document.querySelector("body").addEventListener("keyup", e => {
         if (e.altKey) {
             switch (e.code) {
                 case "ArrowUp":
@@ -32,42 +59,50 @@ document.addEventListener("DOMContentLoaded", function () {
                     break;
             }
 
-            input.dispatchEvent(new KeyboardEvent("keyup", { key: "" }));
-            input.style.fontSize = `${fontSize}px`;
-            input.style.lineHeight = `${lineHeight}px`;
+            textarea.dispatchEvent(new KeyboardEvent("keyup", { key: "" }));
+            textarea.style.fontSize = `${fontSize}px`;
+            textarea.style.lineHeight = `${lineHeight}px`;
         }
     });
 
-    const canvasHeight = canvas.height;
-    const inputHeight = input.offsetHeight;
-
-    const loadImage = (url) =>
-        new Promise((r) => {
+    /**
+     * Load image
+     * @param {string} url URL
+     * @return {Promise}
+     */
+    const loadImage = url =>
+        new Promise(resolve => {
             const img = new Image();
-            img.onload = () => r(img);
+            img.onload = () => resolve(img);
             img.src = url;
         });
 
-    const getPath = (relativePath) => {
+    /**
+     * Get path
+     *
+     * @param {string} relativePath
+     * @return {string}
+     */
+    const getPath = relativePath => {
         if ("string" !== typeof relativePath) {
             relativePath = "";
         }
         return window.location.origin + "/post/" + (relativePath ? relativePath : "");
     };
 
+    // Register service worker
     if ("serviceWorker" in navigator) {
-        // Register service worker
         navigator.serviceWorker
             .register(getPath("pwa.js"), {
                 scope: getPath(),
-                useCache: true,
+                useCache: true
             })
             .then(() => {})
             .catch(() => {});
     }
 
+    // Add the manifest
     (() => {
-        // Add the manifest
         const linkObject = document.getElementById("pwa_manifest");
         null !== linkObject &&
             null === linkObject.getAttribute("href") &&
@@ -91,39 +126,36 @@ document.addEventListener("DOMContentLoaded", function () {
                                         src: getPath("64.png"),
                                         sizes: "64x64",
                                         type: "image/png",
-                                        purpose: "maskable any",
+                                        purpose: "maskable any"
                                     },
                                     {
                                         src: getPath("192.png"),
                                         sizes: "192x192",
                                         type: "image/png",
-                                        purpose: "maskable any",
+                                        purpose: "maskable any"
                                     },
                                     {
                                         src: getPath("512.png"),
                                         sizes: "512x512",
                                         type: "image/png",
-                                        purpose: "maskable any",
-                                    },
+                                        purpose: "maskable any"
+                                    }
                                 ],
                                 display: "fullscreen",
-                                orientation: "landscape",
-                            }),
+                                orientation: "landscape"
+                            })
                         ],
                         {
-                            type: "application/json",
+                            type: "application/json"
                         }
                     )
                 )
             );
     })();
 
-    // Prepare the writable area
-    const inputWidth = input.offsetWidth - 2 * inputPadding - 18;
-
-    // Context ready
+    // Draw on keyup
     if (canvas.getContext) {
-        let ctx = canvas.getContext("2d");
+        const ctx = canvas.getContext("2d");
         ctx.font = `${fontSize}px Oswald`;
 
         (async () => {
@@ -131,17 +163,18 @@ document.addEventListener("DOMContentLoaded", function () {
             const footer = await loadImage("./footer.png");
             const background = await loadImage("./background.svg");
 
-            input.addEventListener("keyup", () => {
+            textarea.addEventListener("keyup", () => {
                 ctx.font = `${fontSize}px Oswald`;
                 const lines = [];
-                const paragraphs = input.value.split("\n");
+                const paragraphs = textarea.value.split("\n");
+                const textareaWidth = textarea.offsetWidth - 2 * textareaPadding - 18;
 
-                paragraphs.forEach((line) => {
+                paragraphs.forEach(line => {
                     let j = 0;
                     for (let i = 0; i < line.length - 1; i++) {
                         const segmentText = line.substring(j, i + 1);
                         const segmentLength = ctx.measureText(segmentText);
-                        if (segmentLength.width >= inputWidth) {
+                        if (segmentLength.width >= textareaWidth) {
                             lines.push(segmentText);
                             j = i + 1;
                         }
@@ -149,32 +182,61 @@ document.addEventListener("DOMContentLoaded", function () {
                     lines.push(line.substring(j));
                 });
 
-                const newInputHeight = inputHeight + (lines.length - 1) * lineHeight;
-                const newCanvasHeight = canvasHeight + (newInputHeight > startStretch ? newInputHeight - startStretch : 0);
-                const marginTop = inputTop + lineHeight - 8;
+                const newTextareaHeight = textareaHeight + (lines.length - 1) * lineHeight;
+                const newCanvasHeight = canvasHeight + (textareaStretch ? newTextareaHeight - 2 * lineHeight : 0);
+                const marginTop = textareaTop + lineHeight - 8;
 
-                canvas.style.height = `${newCanvasHeight}px`;
-                input.style.height = `${newInputHeight}px`;
+                canvas.height = newCanvasHeight;
+                if (canvas.height != canvasHeight) {
+                    canvas.style.height = `${newCanvasHeight}px`;
+                } else {
+                    canvas.style.height = null;
+                }
+                textarea.style.height = `${newTextareaHeight}px`;
 
                 ctx.canvas.height = newCanvasHeight;
-                ctx.fillStyle = "#fff";
-                ctx.fillRect(0, 0, canvas.width, newCanvasHeight);
+                ctx.fillStyle = canvasBkg;
+                ctx.fillRect(0, 0, canvasWidth, newCanvasHeight);
                 ctx.drawImage(background, -560, -560);
                 ctx.drawImage(banner, 0, 50);
-                ctx.drawImage(footer, 0, newCanvasHeight - 200);
+                ctx.drawImage(footer, canvasWidth - 960, newCanvasHeight - 200);
 
-                ctx.fillStyle = "#000";
-                ctx.strokeStyle = "#fff";
+                ctx.fillStyle = canvasColor;
+                ctx.strokeStyle = canvasBkg;
                 ctx.lineWidth = 6;
                 ctx.font = `${fontSize}px Oswald`;
                 lines.map((line, index) => {
-                    ctx.strokeText(line, inputLeft, marginTop + index * lineHeight);
-                    ctx.fillText(line, inputLeft, marginTop + index * lineHeight);
+                    ctx.strokeText(line, textareaLeft, marginTop + index * lineHeight);
+                    ctx.fillText(line, textareaLeft, marginTop + index * lineHeight);
                 });
             });
 
-            input.focus();
-            input.dispatchEvent(new KeyboardEvent("keyup", { key: "" }));
+            textarea.focus();
+            textarea.dispatchEvent(new KeyboardEvent("keyup", { key: "" }));
         })();
     }
+
+    /**
+     * Re-calculate style and update canvas
+     */
+    const reRender = () => {
+        canvas.style.height = null;
+        window.setTimeout(() => {
+            recalculateStyle();
+            textarea.focus();
+            textarea.dispatchEvent(new KeyboardEvent("keyup", { key: "" }));
+        }, 50);
+    };
+
+    // Format change
+    for (const radioElement of [...document.querySelectorAll(".formats input")]) {
+        radioElement.addEventListener("change", e => {
+            holder.setAttribute("data-format", e.target.value);
+            window.setTimeout(reRender, 100);
+        });
+    }
+    strechCheckbox.addEventListener("change", e => {
+        textareaStretch = e.target.checked;
+        window.setTimeout(reRender, 100);
+    });
 });
